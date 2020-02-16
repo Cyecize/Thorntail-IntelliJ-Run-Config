cc4337fc190a6ad53d8b2daf17a039e4aa75a750
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-02-16 19:40:49 +0200

    Added Throntail Runner config.

--- src/main/java/com/cyecize/demo/RestApplication.java
+++ src/main/java/com/cyecize/demo/RestApplication.java
@@ -2,8 +2,12 @@ package com.cyecize.demo;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.wildfly.swarm.runner.Runner;
 
 @ApplicationPath("/api")
 public class RestApplication extends Application {
 
+  public static void main(String[] args) throws Exception {
+    Runner.main(args);
+  }
 }
